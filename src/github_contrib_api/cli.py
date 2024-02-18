da6eacc38795193c6ed47f3b42d4a547b2a19ab0
Jeongseop Lim <jseoplim@gmail.com>
2024-02-18 16:29:17 +0900

    feat: Make schema for cli commands

---
+++ src/github_contrib_api/cli.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from typing import Annotated
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def repo(
+    owner_name: list[str],
+    github_token: Annotated[
+        str, typer.Option(envvar="GITHUB_TOKEN", prompt=True, hide_input=True)
+    ],
+    start_date: Annotated[datetime, typer.Option(formats=["%Y-%m-%d"])],
+    end_date: Annotated[datetime, typer.Option(formats=["%Y-%m-%d"])],
+):
+    """Get a list of pushed repository names between start-date and end-date."""
+    print(owner_name, github_token, start_date, end_date)  # TODO: Remove this line
+
+
+@app.command()
+def pr():  # TODO: Add command arguments and options
+    """Get a list of merged PR counts between start-date and end-date."""
+
+
+def main() -> None:
+    app()
