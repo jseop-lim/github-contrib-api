5f021ec6242b02f8a6664d2f96dcc883bc8d95b8
Jeongseop Lim <jseoplim@gmail.com>
2024-03-09 21:17:04 +0900

    feat: Define callback for csv output validation

--- src/github_contrib_api/callbacks.py
+++ src/github_contrib_api/callbacks.py
@@ -1,7 +1,10 @@
+from pathlib import Path
+
 import typer
 
 
 def repos_callback(ctx: typer.Context, value: list[str]) -> list[str]:
+    # TODO: Separate owner and repos into input-only functions
     owner: str = ctx.params.get("owner", "")
     invalid_repos: list[str] = [
         repo
@@ -20,4 +23,10 @@ def owner_callback(value: str) -> str:
     if value and value.count("/") > 0 or value.startswith("/") or value.endswith("/"):
         raise typer.BadParameter(value)
 
+    return value
+
+
+def csv_output_callback(value: Path | None) -> Path | None:
+    if value and value.suffix != ".csv":
+        raise typer.BadParameter("Must be a CSV file")
     return value
