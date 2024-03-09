61abf75f6f27ada10d502b4aea2b14f294c2a930
Jeongseop Lim <jseoplim@gmail.com>
2024-03-09 21:50:48 +0900

    fix: Modify logic and input of review counter

--- src/github_contrib_api/apps.py
+++ src/github_contrib_api/apps.py
@@ -90,8 +90,7 @@ async def get_merged_pr_count(
 
 
 async def get_pr_review_count(
-    owner_name: str,
-    repo_names: list[str],
+    repo_tuple: Repository,
     github_token: str,
     start_datetime: datetime,
     end_datetime: datetime,
@@ -103,83 +102,78 @@ async def get_pr_review_count(
     - requested: 각 팀원이 2023년에 회사 GitHub 조직의 모든 저장소에 대해 리뷰를 요청 받은 PR 수
     """
     base_url = "https://api.github.com"
+    headers = {"Authorization": f"token {github_token}"}
+
+    owner_name, repo_name = repo_tuple
     review_counts = {}
     reviewed_pr_counts = {}
     review_requested_counts = {}
-    headers = {"Authorization": f"token {github_token}"}
+    count = 0
+    earliest_created_at = end_datetime
+    page = 1
 
     async with ClientSession() as session:
-        for repo_name in repo_names:
-            count = 0
-            earliest_created_at = end_datetime
-            page = 1
-            while earliest_created_at >= start_datetime:
-                prs = await fetch(
+        while earliest_created_at >= start_datetime:
+            prs = await fetch(
+                session=session,
+                url=f"{base_url}/repos/{owner_name}/{repo_name}/pulls",
+                headers=headers,
+                params={
+                    "state": "all",
+                    "sort": "created",
+                    "direction": "desc",
+                    "page": page,
+                    "per_page": 100,
+                },
+            )
+
+            if not prs or earliest_created_at < start_datetime:
+                break
+
+            for pr in prs:
+                if not (
+                    start_datetime <= parse_datetime(pr["created_at"]) <= end_datetime
+                ):
+                    continue
+
+                reviews = await fetch(
                     session=session,
-                    url=f"{base_url}/repos/{owner_name}/{repo_name}/pulls",
+                    url=(
+                        f"{base_url}/repos/{owner_name}/{repo_name}"
+                        f"/pulls/{pr['number']}/reviews"
+                    ),
                     headers=headers,
                     params={
-                        "state": "all",
-                        "sort": "created",
-                        "direction": "desc",
-                        "page": page,
+                        "page": 1,
                         "per_page": 100,
                     },
                 )
 
-                if (
-                    not prs
-                    or not prs[-1]["created_at"]
-                    or earliest_created_at < start_datetime
-                ):
-                    break
+                for review in reviews:
+                    reviewer = review["user"]["login"]
+                    # 본인이 작성한 PR에 단 댓글은 제외
+                    if reviewer != pr["user"]["login"]:
+                        if reviewer not in review_counts:
+                            review_counts[reviewer] = 0
+                        review_counts[reviewer] += 1
+                        count += 1
 
-                for pr in prs:
-                    if not (
-                        start_datetime
-                        <= parse_datetime(pr["created_at"])
-                        <= end_datetime
-                    ):
-                        continue
+                for reviewer in {review["user"]["login"] for review in reviews}:
+                    if reviewer != pr["user"]["login"]:
+                        if reviewer not in reviewed_pr_counts:
+                            reviewed_pr_counts[reviewer] = 0
+                        reviewed_pr_counts[reviewer] += 1
 
-                    reviews = await fetch(
-                        session=session,
-                        url=(
-                            f"{base_url}/repos/{owner_name}/{repo_name}"
-                            f"/pulls/{pr['number']}/reviews"
-                        ),
-                        headers=headers,
-                        params={
-                            "page": 1,
-                            "per_page": 100,
-                        },
-                    )
+                for requested_reviewer in pr.get("requested_reviewers", []):
+                    reviewer = requested_reviewer["login"]
+                    if reviewer not in review_requested_counts:
+                        review_requested_counts[reviewer] = 0
+                    review_requested_counts[reviewer] += 1
 
-                    for review in reviews:
-                        reviewer = review["user"]["login"]
-                        # 본인이 작성한 PR에 단 댓글은 제외
-                        if reviewer != pr["user"]["login"]:
-                            if reviewer not in review_counts:
-                                review_counts[reviewer] = 0
-                            review_counts[reviewer] += 1
-                            count += 1
+            earliest_created_at = parse_datetime(prs[-1]["created_at"])
+            page += 1
 
-                    for reviewer in {review["user"]["login"] for review in reviews}:
-                        if reviewer != pr["user"]["login"]:
-                            if reviewer not in reviewed_pr_counts:
-                                reviewed_pr_counts[reviewer] = 0
-                            reviewed_pr_counts[reviewer] += 1
-
-                    for requested_reviewer in pr.get("requested_reviewers", []):
-                        reviewer = requested_reviewer["login"]
-                        if reviewer not in review_requested_counts:
-                            review_requested_counts[reviewer] = 0
-                        review_requested_counts[reviewer] += 1
-
-                earliest_created_at = parse_datetime(prs[-1]["created_at"])
-                page += 1
-            print(f"repo: {repo_name:32} | page: {page:4} | count: {count:4}")
-
+    print(f"repo: {repo_name:32} | page: {page:4} | count: {count:4}")
     return {
         "review": review_counts,
         "pr": reviewed_pr_counts,
